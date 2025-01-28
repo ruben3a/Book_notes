c5c38e20f58b45392750553a16417673fb19a274
ruben <rubenjosearaujo@icloud.com>
2025-01-28 19:34:37 +0000

    started to test the api

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-import express from "express";
+import express, { request } from "express";
 import pg from "pg";
 import axios from "axios";
 import bodyParser from "body-parser";
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 const app = express();
 const db = new pg.Client({});
 const API = "https://covers.openlibrary.org/b/isbn/$value-S.jpg";
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://covers.openlibrary.org/b/isbn/OL1317211W-S.jpg"
+    );
+    console.log(result.data);
+    res.send(result.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(port);
